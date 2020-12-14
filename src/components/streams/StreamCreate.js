bc138ec68e8b6f73c0a0b9fe57be492db2ef6b5d
Jake <neelunstp@gmail.com>
2020-12-14 20:43:08 +0530

    redux form: 5th phase

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,86 +1,71 @@
 import React from 'react';
 import { Field, reduxForm } from "redux-form";
 
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button'
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
+      width: '25ch'
+    }
+  }
 }));
 
 
-const renderedBasicInputField = ({
-  input, 
-  meta: { error, invalid, touched }
-}) => {
-  return ( 
-    <TextField 
-      id="outlined-basic" 
-      error={touched && invalid}
-      helperText={touched && error}
-      label="Name" 
-      variant="outlined"
-      {...input}
-    /> 
-  )
-};
-
-const renderedMultilineField = ({
-  input, 
-  meta: {error, invalid, touched}
-}) => {
+const renderTextField = (fieldProps) => {
+  const {input, multiline, rows} = fieldProps;
+  const {invalid, touched, error} = fieldProps.meta;
+  console.log("fieldProps", fieldProps);
   return (
     <TextField 
-      {...input}
-      id="outlined-multiline-static" 
-      label="Description" 
-      multiline
-      rows={5}
-      variant="outlined" 
-      error = {touched && invalid}
-      helperText={touched && error}
-    /> 
+    variant="outlined"
+    label={input.name}
+    multiline = {multiline}
+    rows = {rows}
+    error={touched && invalid}
+    helperText={touched && error}
+    {...input}
+    />
   )
-};
+}
 
+//*********** COMPONENT STARTS HERE **************/
 
-//******* COMPONENT STARTS HERE ********/
 const StreamCreate = (props) => {
-  const {handleSubmit, pristine, reset, submitting} = props;
+  const { handleSubmit } = props;
   const classes = useStyles();
 
-  const onFormSubmit = (formValues) => {
-    console.log(formValues)
-  };
+  const onFormSubmit = (formValue) => {
+    console.log("submit", formValue);
+  }
+  console.log("formProps", props);
 
   return (
-    <form 
-      className={classes.root} 
-      noValidate 
-      autoComplete="off"
-      onSubmit={handleSubmit(onFormSubmit)}
-    >
-        <Field name="title" component={renderedBasicInputField} /><br />
-        <Field name="description" component={renderedMultilineField}/><br/>
-        <Button variant="contained" color="primary" type="submit">Submit</Button>
+    <form className={classes.root} onSubmit={handleSubmit(onFormSubmit)}>
+      <Field name="title" component={renderTextField} /> <br />
+      <Field 
+        name="description" 
+        component={renderTextField} 
+        multiline={true} 
+        rows={4}
+      />
+      <br />
+      <Button variant="contained" color="primary" type="submit">Submit</Button>
     </form>
-  );
-};
+  )
+}
 
 const validate = (formValues) => {
-  const errors= {};
-  if (!formValues.title) {
-    errors.title = "You must enter a title"; 
+  const errors = {};
+  if(!formValues.title) {
+    errors.title = "You must enter a title"
   }
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
+  if(!formValues.description) {
+    errors.description="you must enter a description"
   }
   return errors;
 }
@@ -89,9 +74,3 @@ export default reduxForm({
   form: "streamCreate",
   validate: validate
 })(StreamCreate);
-
-
-//react hook form
-//react final form
-// redux form
-//formik and yup
