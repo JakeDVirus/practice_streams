83ff44d4c0f411de85434d030e65ba38b4650295
Jake <neelunstp@gmail.com>
2020-12-17 18:50:16 +0530

    phase 2

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider'
 import AccountBoxTwoToneIcon from '@material-ui/icons/AccountBoxTwoTone';
 import Typography from "@material-ui/core/Typography";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import indigo from '@material-ui/core/colors/indigo';
 
 import { fetchStreams } from "../../actions/index";
 
@@ -24,7 +27,25 @@ const useStyles = makeStyles((theme) => ({
   },
   list: {
     marginTop: "30px",
-    backgroundColor: theme.palette.background.paper
+  },
+  listItem: {
+    maxWidth: "380px",
+    flexDirection: "column",
+    backgroundColor: indigo[50],
+    marginTop: "5px"
+  },
+  listItem__main: {
+    display: "flex"
+  }, 
+  listItem__footer: {
+    alignSelf: "flex-end"
+  },
+  avatar: {
+    color: indigo[400],
+    backgroundColor: indigo[100]
+  },
+  button: {
+    color: indigo[300]
   }
 }));
 
@@ -37,18 +58,24 @@ const StreamList = ({streams, fetchStreams}) => {
   
   const renderStreamsList = streams.map(stream => (
     <React.Fragment>
-      <ListItem key={stream.id} alignItems="flexStart">
-        <ListItemAvatar>
-          <Avatar>
-            <AccountBoxTwoToneIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={stream.title}
-          secondary={stream.description}
-        />
+      <ListItem key={stream.id} alignItems="flexStart" className={classes.listItem}>
+        <div className={classes.listItem__main}>
+          <ListItemAvatar>
+            <Avatar className={classes.avatar}>
+              <AccountBoxTwoToneIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={stream.title} secondary={stream.description} />
+        </div>
+        <div className={classes.listItem__footer}>
+          <IconButton  edge="end" className={classes.button}>
+            <EditIcon />
+          </IconButton>
+          <IconButton edge="end" className={classes.button}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
       </ListItem>
-      <Divider component="li" />
     </React.Fragment>
   ));
 
